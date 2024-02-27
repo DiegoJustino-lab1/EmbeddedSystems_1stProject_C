8542dc69b2fae9de17fbd86d52c4484d82bb6ca5
Diego Justino <DiegoJsilva01@outlook.com>
2024-02-27 16:21:55 +0000

    first

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+
+int main() {
+    unsigned int mask_pin18 = 1 << 18;
+
+    volatile unsigned int *port1_set = (unsigned int *)0x2009C038;
+    volatile unsigned int *port1_clr = (unsigned int *)0x2009C03C;
+
+    while (true) { 
+        *port1_set |= mask_pin18;
+        ThisThread::sleep_for(500ms); // Sleep for 500 milliseconds (0.5 seconds)
+    }
+    
+}
